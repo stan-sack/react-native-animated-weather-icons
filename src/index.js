81e332b37449445aa408a708071cf6ffcfecaf52
Stan Tsouvallas <stsouvallas@snapchat.com>
2018-02-11 11:37:43 +1100

    initial commit

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import ThunderIcon from './ThunderIcon'
+export ThunderIcon
+
+import SunIcon from './SunIcon'
+export SunIcon
+
+import SunCloudIcon from './SunCloudIcon'
+export SunCloudIcon
+
+import WindIcon from './WindIcon'
+export WindIcon
+
+import SnowIcon from './SnowIcon'
+export SnowIcon
+
+import RainIcon from './RainIcon'
+export RainIcon
